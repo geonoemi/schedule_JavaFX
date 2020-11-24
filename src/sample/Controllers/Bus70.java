1ad09d379a426732c0ac2e35f382a7ba7a358f18
Noémi László <geonoemilaszlo@gmail.com>
2020-11-24 14:08:13 +0100

    debugged bus scenes

--- src/sample/Controllers/Bus70.java
+++ src/sample/Controllers/Bus70.java
@@ -1,10 +1,19 @@
 package sample.Controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class Bus70 {
+    @FXML
+    Button stat1;
+    @FXML
+    Button stat2;
+    @FXML
+    Button stat3;
 
     public void stat1Click(ActionEvent actionEvent) {
+
     }
 
     public void stat2Click(ActionEvent actionEvent) {
